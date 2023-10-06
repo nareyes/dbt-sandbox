3570d99debfdefc824c69bd458f377c6e10bc5c4
Nick Reyes <dbtsandbox@pm.me>
2023-10-06 15:14:27 +0000

    created all dates model

---
+++ models/marts/core/dim_all_dates.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized = 'table'
+)}}
+
+select
+    date_day,
+    date_trunc('month', date_day) as first_day_of_month,
+    last_day(date_day) as last_day_of_month
+from {{ ref('dim_date_spine') }}
